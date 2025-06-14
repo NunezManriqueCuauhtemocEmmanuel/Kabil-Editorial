6e471d9ff0093d276ad6c17a2bf333e4bd68cc3d
NunezManriqueCuauhtemocEmmanuel <cnunezm1900@alumno.ipn.mx>
2025-06-14 11:24:53 -0600

    Primera Update

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,22 @@ const main = document.querySelector('main');
         toggleBtn.style.display = 'inline-block';
         logo.style.display = 'none';
     });
+
+ window.addEventListener('scroll', () => {
+        const header = document.querySelector('header');
+        const main = document.querySelector('main');
+        
+        const mainRect = main.getBoundingClientRect();
+        const mainTop = mainRect.top + window.scrollY;
+        const mainHeight = main.offsetHeight;
+        const scrollY = window.scrollY;
+
+        // Calcular 70% de la posición del main
+        const triggerPoint = mainTop + mainHeight * 0.25;
+
+        if (scrollY >= triggerPoint) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
